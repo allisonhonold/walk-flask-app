8f7861bbb922f4422dac96c47b084b2dc105edec
Allison Honold <allisonhonold@gmail.com>
2019-11-12 10:39:13 -0800

    add powered by, privacy, terms of use

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, send_from_directory, render_template, request, abort
 from waitress import serve
 from models.arrest_predictor import predict_arrest
+import time
+
+import sys
+sys.path.insert(1, '../walk_risk_engine')
+from results import get_backend_results
 
 app = Flask(__name__, static_url_path="/static")
 
@@ -14,15 +19,32 @@ def get_results():
     """ Predict the probability of 1+ arrests at this location today based on 
     the inputs. """
     data = request.form
-    print(data)
 
     expected_features = ("start_lat", "start_long", "end_lat", "end_long")
 
     if data and all(feature in data for feature in expected_features):
         # Convert the dict of fields into a list
-        test_value = [float(data[feature]) for feature in expected_features]
-        predicted_class = predict_arrest(test_value)
-        return render_template("results.html", predicted_class=predicted_class)
+        # test_value = [float(data[feature]) for feature in expected_features]
+
+        m, risk, warning = get_backend_results(str(data['start_lat']), 
+                                                str(data['start_long']),
+                                                str(data['end_lat']),
+                                                str(data['end_long'])
+                                                )
+
+        new_map_name = ("results_map_" + str(round(time.time())) + ".html")
+
+        m.save('static/' + new_map_name)
+        
+        # risk = (1, 'low')
+        # warning = 'Google Warning'
+        # new_map_name = 'results_map_1573495916.762036.html'
+
+        return render_template("results.html", 
+                                risk=risk[1],
+                                goog_warning=warning,
+                                map_file_name=new_map_name
+                                )
     else:
         return abort(400)
 
@@ -31,6 +53,8 @@ def get_results():
         # cast to float
     # except:
         # return abort(400) (or make a error page)
+
+    
     
 
 if __name__ == "__main__":
